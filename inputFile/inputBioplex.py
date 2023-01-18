5a32149b7e5a873c74eaf6c848d836e1a5edbbf1
wasinee_dev <wasinees@gmail.com>
2023-01-18 14:37:24 +0700

    wget bioplex data

--- inputFile/inputBioplex.py
+++ inputFile/inputBioplex.py
@@ -10,12 +10,11 @@ class CInputBioplex:
         super().__init__()
 
         df = pd.read_csv(filePath, sep='\t')
-        #df_filtered = df[df.apply(lambda x: not x['bait_symbol'].isnumeric() and x['bait_symbol'] != "nan", axis=1)]
-        #df_filtered = df_filtered[df_filtered.apply(lambda x: isinstance(x['symbol'], str) and not x['symbol'].isnumeric(), axis=1)]
+        df_filtered = df[df.apply(lambda x: not x['bait_symbol'].isnumeric() and x['bait_symbol'] != "nan", axis=1)]
+        df_filtered = df_filtered[df_filtered.apply(lambda x: isinstance(x['symbol'], str) and not x['symbol'].isnumeric(), axis=1)]
 
-        df_filtered = df
-        bait_list = np.array(df_filtered['Bait Symbol'], dtype='U21')
-        prey_list = np.array(df_filtered['Prey Symbol'], dtype='U21')
+        bait_list = np.array(df_filtered['bait_symbol'], dtype='U21')
+        prey_list = np.array(df_filtered['symbol'], dtype='U21')
         
         proteins_list = np.append(bait_list, prey_list)
             
